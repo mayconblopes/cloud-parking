2528867558ced4368475151d708a23d114bab97c
Lopes, Maycon <lopes.maycon@gmail.com>
2022-08-05 08:02:06 -0300

    Initial commit

---
+++ src/test/java/com/mike/parking/CloudParkingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mike.parking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudParkingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
